8f37a90c8a4460d776bc6f3ae1c323b1c4467756
Yaugen <eugene@novoroll.com>
2019-12-22 12:41:34 +0300

    add 22

--- 22/02.js
+++ 22/02.js
@@ -11,12 +11,12 @@ const getShuffleOrder = inputPath => {
       let shuffle = [];
       if ((match = line.match(/cut (-?\d+)/))) {
         const n = Number(match[1]);
-        shuffle = ["cut", n];
+        shuffle = ["cut", BigInt(n)];
       } else if ((match = line.match(/new stack/))) {
         shuffle = ["new stack"];
       } else if ((match = line.match(/increment (-?\d+)/))) {
         const n = Number(match[1]);
-        shuffle = ["increment", n];
+        shuffle = ["increment", BigInt(n)];
       }
       return shuffle;
     });
@@ -25,29 +25,20 @@ const getShuffleOrder = inputPath => {
 const mod = (a, b) => ((a % b) + b) % b;
 
 const egcd = (a, b) => {
-  if (a === 0) {
-    return [b, 0, 1];
+  if (a === 0n) {
+    return [b, 0n, 1n];
   } else {
     const [g, y, x] = egcd(mod(b, a), a);
-    return [g, x - Math.floor(b / a) * y, y];
+    return [g, x - (b / a) * y, y];
   }
 };
 
 const modInv = (a, m) => {
   const [g, x, y] = egcd(a, m);
-  if (g !== 1) {
+  if (g !== 1n) {
     throw new Error("modular inverse does not exist");
   } else {
     return mod(x, m);
-  }
-};
-
-const expmod = (base, exp, mod) => {
-  if (exp == 0) return 1;
-  if (exp % 2 == 0) {
-    return Math.pow(expmod(base, exp / 2, mod), 2) % mod;
-  } else {
-    return (base * expmod(base, exp - 1, mod)) % mod;
   }
 };
 
@@ -66,15 +57,15 @@ const modPow = (a, b, n) => {
 
 const run = inputPath => {
   const shuffleOrder = getShuffleOrder(inputPath);
-  const N = 119315717514047;
-  const repeats = 101741582076661;
+  const N = 119315717514047n;
+  const repeats = 101741582076661n;
 
-  let cards = [1, 0];
+  let cards = [1n, 0n];
 
   shuffleOrder.forEach(([shuffleType, n]) => {
     const [a, b] = cards;
     if (shuffleType === "new stack") {
-      cards = [mod(-a, N), mod(-b - 1, N)];
+      cards = [mod(-a, N), mod(-b - 1n, N)];
     } else if (shuffleType === "cut") {
       cards = [a, mod(b - n, N)];
     } else if (shuffleType === "increment") {
@@ -83,12 +74,12 @@ const run = inputPath => {
   });
 
   const [a, b] = cards;
-  const an = Number(modPow(BigInt(a), BigInt(repeats), BigInt(N)));
+  const an = modPow(BigInt(a), BigInt(repeats), BigInt(N));
 
   const A = an;
-  const B = b * (an - 1) * modInv(a - 1, N);
+  const B = b * (an - 1n) * modInv(a - 1n, N);
   console.log(A, B);
-  console.log(mod((2020 - B) * modInv(A, N), N));
+  console.log(mod((2020n - B) * modInv(A, N), N));
 };
 
 run("./input.txt");
